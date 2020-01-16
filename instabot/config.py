8259aac2371c7e00d9d2cdad822aaf0456f9ffb3
xrustle <dbatorov@gmail.com>
2020-01-16 09:44:12 +0300

    init classes

---
+++ instabot/config.py
@@ -0,0 +1,9 @@
+import json
+
+with open('../config.json') as json_data_file:
+    conf = json.load(json_data_file)
+
+USER = conf['credentials']['username']
+PASSWORD = conf['credentials']['password']
+
+TOKEN = conf['token']
---
+++ instabot/insta.py
@@ -0,0 +1,34 @@
+from instaloader import Instaloader, Profile, TwoFactorAuthRequiredException
+from instabot.config import *
+
+
+class insta:
+    def __init__(self):
+        self.loader = Instaloader()
+        try:
+            self.loader.load_session_from_file(USER, f'session-{USER}')
+        except FileNotFoundError:
+            self.loader.context.log("Session file does not exist yet - Logging in.")
+        if not self.loader.context.is_logged_in:
+            try:
+                self.loader.login(USER, PASSWORD)
+            except TwoFactorAuthRequiredException:
+                self.loader.twgio_factor_login(input('Code: '))
+            self.loader.save_session_to_file(f'session-{USER}')
+        if self.loader.context.is_logged_in:
+            self.loader.context.log('Logged in.', end='\n' * 2)
+
+    def get_unfollowers(self, user) -> list:
+        self.loader.context.log('Getting list of accounts i\'m subscribed to but not subscribed to me:')
+        profile = Profile.from_username(self.loader.context, user)
+
+        followers = profile.get_followers()
+        followees = profile.get_followees()
+
+        unfollowers = set(followees).difference(set(followers))
+        unfollowers_list = []
+
+        for unfollower in unfollowers:
+            unfollowers_list.append(f'{unfollower.full_name} @{unfollower.username}')
+
+        return unfollowers_list
