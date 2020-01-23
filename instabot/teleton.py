7892f69b1fae4942ab03e1497810905aedb4c33a
Dmitry <31348332+xrustle@users.noreply.github.com>
2020-01-23 18:13:07 +0300

    Telethon First Steps

--- instabot/teleton.py
+++ instabot/teleton.py
@@ -1,36 +1,71 @@
-from instabot.config import API
-from telethon import TelegramClient
-from telethon.tl.types.input_peer_chat import InputPeerChat
+from instabot.config import API, MTPROTO
+from telethon import TelegramClient, connection
+import logging
 
-# API config: https://my.telegram.org/apps
-# Official examples:
-# https://github.com/LonamiWebs/Telethon/blob/0e38ab4/telethon_examples/interactive_telegram_client.py
-# Docs: https://telethon.readthedocs.io/en/latest/basic/quick-start.html
-# https://habr.com/ru/post/425151/
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 
-chat_id = 844216
+client = TelegramClient(
+    'session-telegram', **API,
+    connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
+    proxy=tuple(MTPROTO)
+)
 
-client = TelegramClient('session-telethon', **API)
-client.connect()
-chat = InputPeerChat(chat_id)
 
-total_count, messages, senders = client.get_message_history(chat, limit=10)
+async def main():
+    # Getting information about yourself
+    me = await client.get_me()
 
-for msg in reversed(messages):
-    # Format the message content
-    if getattr(msg, 'media', None):
-        content = '<{}> {}'.format(  # The media may or may not have a caption
-        msg.media.__class__.__name__,
-        getattr(msg.media, 'caption', ''))
-    elif hasattr(msg, 'message'):
-        content = msg.message
-    elif hasattr(msg, 'action'):
-        content = str(msg.action)
-    else:
-        # Unknown message, simply print its class name
-        content = msg.__class__.__name__
+    # "me" is an User object. You can pretty-print
+    # any Telegram object with the "stringify" method:
+    print(me.stringify())
 
-    text = '[{}:{}] (ID={}) {}: {} type: {}'.format(
-            msg.date.hour, msg.date.minute, msg.id, "no name",
-            content)
-    print(text)
+    # When you print something, you see a representation of it.
+    # You can access all attributes of Telegram objects with
+    # the dot operator. For example, to get the username:
+    username = me.username
+    print(username)
+    print(me.phone)
+
+    # You can print all the dialogs/conversations that you are part of:
+    async for dialog in client.iter_dialogs():
+        print(dialog.name, 'has ID', dialog.id)
+
+    # You can send messages to yourself...
+    await client.send_message('me', 'Hello, myself!')
+    # ...to some chat ID
+    # await client.send_message(-100123456, 'Hello, group!')
+    # ...to your contacts
+    # await client.send_message('+34600123123', 'Hello, friend!')
+    # ...or even to any username
+    # await client.send_message('TelethonChat', 'Hello, Telethon!')
+
+    # You can, of course, use markdown in your messages:
+    message = await client.send_message(
+        'me',
+        'This message has **bold**, `code`, __italics__ and '
+        'a [nice website](https://lonamiwebs.github.io)!',
+        link_preview=False
+    )
+
+    # Sending a message returns the sent message object, which you can use
+    print(message.raw_text)
+
+    # You can reply to messages directly if you have a message object
+    await message.reply('Cool!')
+
+    # Or send files, songs, documents, albums...
+    # await client.send_file('me', '/home/me/Pictures/holidays.jpg')
+
+    # You can print the message history of any chat:
+    async for message in client.iter_messages('me'):
+        print(message.id, message.text)
+
+        # You can download media from messages, too!
+        # The method will return the path where the file was saved.
+        if message.photo:
+            path = await message.download_media()
+            print('File saved to', path)  # printed after download is done
+
+with client:
+    client.loop.run_until_complete(main())
